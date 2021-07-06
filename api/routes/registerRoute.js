d74dfcbe15c94ddc2c11588ea812397cde9c376c
Matheus Gonçalves <matheus96.goncalves@gmail.com>
2021-07-06 13:26:11 -0300

    readme

--- api/routes/registerRoute.js
+++ api/routes/registerRoute.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const RegisterController = require('../controllers/RegisterController')
 const middlewaresAuthentication = require('../security/middlewares-authentication')
-const passport = require('passport')
 
 const router = Router()
 
